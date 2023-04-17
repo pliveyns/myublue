fcae7a7f7970d22da34be4b06312c426ef17c435
pliveyns <pliveyns@users.noreply.github.com>
2023-04-17 20:57:38 +0200

    20230417 Debuging mybuild.sh

--- mybuild.sh
+++ mybuild.sh
@@ -2,11 +2,11 @@
 # Additions to the Silverblue image
 
 # Make sure the fonts directory exists
-mkdir -p /var/usrlocal/share/fonts
+mkdir -p /var/usrlocal/share/fonts/Meslo
 
 # Download the MelsoLGS fonts for system wide us
-wget -O "/usr/local/share/fonts/MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -O "/usr/local/share/fonts/MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -O "/usr/local/share/fonts/MesloLGS NF Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -O "/usr/local/share/fonts/MesloLGS NF Bold Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+wget -O "/usr/local/share/fonts/Meslo/MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget -O "/usr/local/share/fonts/Meslo/MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget -O "/usr/local/share/fonts/Meslo/MesloLGS NF Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget -O "/usr/local/share/fonts/Meslo/MesloLGS NF Bold Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
