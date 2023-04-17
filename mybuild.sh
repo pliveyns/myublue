83b55422d22f261078c67518a925423aa9913fa2
pliveyns <pliveyns@users.noreply.github.com>
2023-04-17 21:46:54 +0200

    20230417 remove fonts from mybuild.sh

--- mybuild.sh
+++ mybuild.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 # Additions to the Silverblue image
 
-# Make sure the fonts directory exists
-#mkdir -p /var/usrlocal/share/fonts/Meslo
-mkdir -p /usr/share/fonts/MesloLGS
-
-# Download the MelsoLGS fonts for system wide us
-wget -O "/usr/share/fonts/MesloLGS/MesloLGS-NF-Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -O "/usr/share/fonts/MesloLGS/MesloLGS-NF-Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -O "/usr/share/fonts/MesloLGS/MesloLGS-NF-Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -O "/usr/share/fonts/MesloLGS/MesloLGS-NF-BoldItalic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-
