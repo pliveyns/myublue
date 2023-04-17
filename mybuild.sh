5b6a4ce97f7c04be14d3ca4fc4c2826c2eb29f9c
pliveyns <pliveyns@users.noreply.github.com>
2023-04-17 20:20:29 +0200

    20230417 Add command to mybuild.sh script to make shure fonts directory exists

--- mybuild.sh
+++ mybuild.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Additions to the Silverblue image
 
+# Make sure the fonts directory exists
+mkdir -p /usr/local/share/fonts
+
 # Download the MelsoLGS fonts for system wide us
 wget -O "/usr/local/share/fonts/MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget -O "/usr/local/share/fonts/MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
